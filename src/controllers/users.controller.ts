729fed3cd957c4249de2df1303975be74d2b1849
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-08-01 16:02:13 -0300

    feat: cria a função de checagem de usuário no banco de dados

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from 'express';
 import usersService from '../services/users.service';
 import { IUser } from '../interfaces/user.interface';
-import createToken from '../helpers/createToken';
+import tokenHelper from '../helpers/createToken';
 
 const create = async (req: Request, res: Response) => {
   try {
     const { username, classe, level, password } = req.body;
     const user: IUser = { username, classe, level, password };
     await usersService.create(user);
-    const token = createToken(user);
+    const token = tokenHelper.createToken(user);
     return res.status(201).json({ token });
   } catch (err) {
     return res.status(500).json(err);
--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,6 +1,17 @@
 import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
 import { IUser } from '../interfaces/user.interface';
+import { ILogin } from '../interfaces/login.interface';
+
+export const getUser = async (user: ILogin): Promise<ILogin> => {
+  const { username, password } = user;
+  const query = `SELECT username, password FROM Trybesmith.Users
+  WHERE username = ? AND password = ?;`;
+  const values = [username, password];
+  const [result] = await connection.execute(query, values);
+  const [returnedUser] = result as ILogin[];
+  return returnedUser;
+};
 
 const create = async (user: IUser): Promise<IUser> => {
   const { username, classe, level, password } = user;
