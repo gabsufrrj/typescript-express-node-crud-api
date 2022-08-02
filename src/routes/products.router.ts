908714160dcadc7fffdd87077526c7c78ec74d41
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-08-02 09:39:41 -0300

    feat: adiciona o middleware na rota de products

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import productsController from '../controllers/products.controller';
+import productsValidation from '../middlewares/productsValidation';
 
 const router = Router();
 
 router.get('/', productsController.getAll);
 
-router.post('/', productsController.create);
+router.post(
+  '/', 
+  productsValidation.nameValidation,
+  productsValidation.amountValidation,
+  productsController.create,
+);
 
 export default router;
